999214a383c4aaae355714739baf5d77e3ed32eb
Janer Vega <janervega399@gmail.com>
2024-10-14 16:35:21 -0500

    BlueJ project started

---
+++ BlueJ/Grupo.java
@@ -0,0 +1,71 @@
+public class Grupo {
+    private String nombre;
+    private String descripcion;
+    private String horario;
+    private String ubicacion;
+    private int cupos;
+    private String facultad;
+    private String escuela;
+
+    public Grupo() {}
+    public Grupo(String nombre, String descripcion, String horario, String ubicacion, int cupos, String facultad, String escuela) {}
+
+    public void almacenar_inscritos() {}
+    public void almacenar_cupos() {}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getCupos() {
+        return cupos;
+    }
+
+    public void setCupos(int cupos) {
+        this.cupos = cupos;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getEscuela() {
+        return escuela;
+    }
+
+    public void setEscuela(String escuela) {
+        this.escuela = escuela;
+    }
+}
---
+++ BlueJ/Usuario.java
@@ -0,0 +1,43 @@
+public class Usuario {
+    private String nombre;
+    private String correo_electronico;
+    private String facultad;
+    private String carrera;
+
+    public Usuario() {}
+
+    public void inscribirse_a_grupo(Grupo grupo) {}
+    public void elimar_inscripcion_a_grupo(Grupo grupo) {}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+}
