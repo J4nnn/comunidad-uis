5a52bc2217ab6480687251b1290d9994798f1c84
Janer Vega <janervega399@gmail.com>
2024-10-16 09:03:27 -0500

    IntelliJ IDEA and SpringBoot

---
+++ src/main/java/com/uis/comunidad/ComunidadUisApplication.java
@@ -0,0 +1,13 @@
+package com.uis.comunidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunidadUisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunidadUisApplication.class, args);
+	}
+
+}
