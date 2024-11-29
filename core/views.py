ed3f618815650c03e0f2aba8676cc04ec212b707
Janer Vega <janervega399@gmail.com>
2024-11-29 16:58:10 -0500

    [Modified] Added views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from core.serializer import *
+from core.models import *
 
 # Create your views here.
+
+class FacultyView(viewsets.ModelViewSet):
+    serializer_class = FacultySerializer
+    queryset = Faculty.objects.all()
+
+class SchoolView(viewsets.ModelViewSet):
+    serializer_class = SchoolSerializer
+    queryset = School.objects.all()
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class GroupView(viewsets.ModelViewSet):
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
